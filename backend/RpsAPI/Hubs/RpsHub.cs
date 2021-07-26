db77cbc88b61500478ab3294cb906c3ab2373379
BlakkBerry <blakkberry2015@gmail.com>
2021-07-26 18:21:05 +0300

    Started asp.net backend

---
+++ backend/RpsAPI/Hubs/RpsHub.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace RpsAPI.Hubs
+{
+    public class RpsHub : Hub
+    {
+        public async Task JoinGame(string code)
+        {
+            await Clients.All.SendAsync("GameJoined");
+        }
+    }
+}
